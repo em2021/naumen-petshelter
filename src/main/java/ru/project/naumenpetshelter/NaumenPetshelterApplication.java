c7afbac999d9116cf14839931a593ea417b25ada
em2021 <egor_m@icloud.com>
2023-09-14 22:28:26 +0300

    Initial commit

---
+++ src/main/java/ru/project/naumenpetshelter/NaumenPetshelterApplication.java
@@ -0,0 +1,13 @@
+package ru.project.naumenpetshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenPetshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaumenPetshelterApplication.class, args);
+	}
+
+}
