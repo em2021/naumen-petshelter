de44c335e884a282e7d7fc8129568f0edd257d72
em2021 <egor_m@icloud.com>
2023-12-20 23:48:01 +0300

    Added DBContext to PetshelterAppConfig

--- src/main/java/ru/project/naumenpetshelter/config/PetshelterAppConfig.java
+++ src/main/java/ru/project/naumenpetshelter/config/PetshelterAppConfig.java
@@ -1,7 +1,10 @@
 package ru.project.naumenpetshelter.config;
 
+import org.mapdb.DBMaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.abilitybots.api.db.DBContext;
+import org.telegram.abilitybots.api.db.MapDBContext;
 import ru.project.naumenpetshelter.utils.AnimalMessageBuilder;
 
 @Configuration
@@ -10,5 +13,10 @@ public class PetshelterAppConfig {
     @Bean
     public AnimalMessageBuilder animalMessageBuilder() {
         return new AnimalMessageBuilder();
+    }
+
+    @Bean
+    public DBContext dbContext() {
+        return new MapDBContext(DBMaker.memoryDB().make());
     }
 }
